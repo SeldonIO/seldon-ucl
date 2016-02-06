fc9fefbc794a4c14c3482c86d38723dc840e421c
gkyc <kwok.cheng.14@ucl.ac.uk>
2016-02-06 22:57:42 +0000

    Fix missing data and replace values visibility rules

--- flaskApp/static/directives/clean.sidebar.missingValues.js
+++ flaskApp/static/directives/clean.sidebar.missingValues.js
@@ -13,7 +13,7 @@ angular.module('dcs.directives').directive('cleanSidebarMissingValues', ['sessio
 		link: function(scope, element, attr) {
 			scope.$watch('tableSelection', function(selection, oldSelection)
 			{
-				scope.shouldShow = typeof selection === 'object' && selection.columns.length == 1;
+				scope.shouldShow = typeof selection === 'object' && selection.columns.length == 1 && scope.tableSelection.rows.length > 1;
 				if(scope.shouldShow && typeof session.dataTypes === 'object' )
 					scope.shouldShowInterpolation = element.interpolationAllowedDataTypes.indexOf(session.dataTypes[selection.columns[0]]) >= 0;
 
