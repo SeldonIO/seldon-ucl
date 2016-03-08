68127886d2629c2f2cb4a2abd41a78b7ae8ff222
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2016-03-08 22:52:08 +0000

    Move Windows provisioning script to correct directory

--- vagrant_provisioning/supervisor/windows.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-source /home/vagrant/venv/bin/activate
-pip install pip --upgrade
-pip install tables --no-binary
-deactivate
---
+++ vagrant_provisioning/windows.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+source /home/vagrant/venv/bin/activate
+echo “Re-installing tables (for Windows)”
+pip install pip --upgrade > /dev/null
+pip uninstall tables > /dev/null
+pip install tables --no-binary > /dev/null
+deactivate
