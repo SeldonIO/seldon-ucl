e38b8268bace8fc27374dca0bf43935ae4d2475a
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2016-03-08 23:03:42 +0000

    Fix syntax error in Windows provisioning script

--- vagrant_provisioning/windows.sh
+++ vagrant_provisioning/windows.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source /home/vagrant/venv/bin/activate
-echo “Re-installing tables (for Windows)”
+echo "Re-installing tables for Windows"
 pip install pip --upgrade > /dev/null
 pip uninstall tables > /dev/null
 pip install tables --no-binary > /dev/null
