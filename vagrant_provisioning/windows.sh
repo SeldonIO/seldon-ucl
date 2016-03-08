7d5a80f6656351fcfb1cd837e0e5fbb96a10b1aa
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2016-03-08 23:09:30 +0000

    Fix incorrect pip install command in Windows provisioning script

--- vagrant_provisioning/windows.sh
+++ vagrant_provisioning/windows.sh
@@ -2,6 +2,6 @@
 source /home/vagrant/venv/bin/activate
 echo "Re-installing tables for Windows"
 pip install pip --upgrade > /dev/null
-pip uninstall tables > /dev/null
-pip install tables --no-binary > /dev/null
+pip uninstall -y tables > /dev/null
+pip install tables --no-binary all > /dev/null
 deactivate
