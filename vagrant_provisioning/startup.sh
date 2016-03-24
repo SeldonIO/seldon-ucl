983075415f30c1660e51e5385565950b9f73dec3
bandienkhamgalan <bandi.enkhamgalan@gmail.com>
2016-03-24 23:13:52 +0000

    Remove redundant Windows provisioning script

--- vagrant_provisioning/startup.sh
+++ vagrant_provisioning/startup.sh
@@ -3,4 +3,4 @@
 echo "Starting web server daemons..."
 service nginx start > /dev/null
 supervisorctl reread > /dev/null
-supervisorctl update > /dev/null
+supervisorctl restart all > /dev/null
--- vagrant_provisioning/windows.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-source /home/vagrant/venv/bin/activate
-echo "Re-installing tables for Windows"
-dos2unix /vagrant/vagrant_provisioning/supervisor/celery.sh
-dos2unix /vagrant/vagrant_provisioning/supervisor/gunicorn.sh
-pip uninstall -y tables > /dev/null
-pip install tables --no-binary all > /dev/null
-deactivate
